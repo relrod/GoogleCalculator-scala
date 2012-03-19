fe873555d3ecea8492a49ca852ea9d7fbd890d9c
Ricky Elrod <ricky@elrod.me>
2012-03-18 23:54:36 -0400

    Use https instead of http, by default, but optionally able to use http.

--- src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
+++ src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
@@ -6,9 +6,10 @@ import org.jsoup.nodes.Document
 object GoogleCalculator {
   System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20120222 Firefox/13.0a1")
 
-  def calculate(query: String): String = {
+  def calculate(query: String, secure: Boolean = true): String = {
     val encodedQuery: String = URLEncoder.encode(query, "UTF-8").replace("+", "%20")
-    val doc: Document = Jsoup.connect("http://www.google.com/search?q=" + encodedQuery).get()
+    val protocol = if (secure) "https" else "http"
+    val doc: Document = Jsoup.connect(protocol + "://www.google.com/search?q=" + encodedQuery).get()
     doc.select("h2.r b").text
   }
 }
