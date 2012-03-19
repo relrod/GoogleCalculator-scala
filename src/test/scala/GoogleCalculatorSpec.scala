0ae139ab07d2c5032ede0cdccc4a9a443a608211
Ricky Elrod <ricky@elrod.me>
2012-03-19 00:21:12 -0400

    Test both secure and nonsecure in all cases.

--- src/test/scala/GoogleCalculatorSpec.scala
+++ src/test/scala/GoogleCalculatorSpec.scala
@@ -4,11 +4,13 @@ import me.elrod.GoogleCalculator._
 class GoogleCalculatorSpec extends FunSpec {
   describe("The library") {
     it("should correctly retrieve the result of 2+2") {
-      assert(GoogleCalculator.calculate("2 + 2") == "2 + 2 = 4")
+      assert(GoogleCalculator.calculate("2 + 2", secure = false) == "2 + 2 = 4")
+      assert(GoogleCalculator.calculate("2 + 2", secure = true) == "2 + 2 = 4")
     }
 
     it("should correctly retrieve an approximation of Pi") {
-      assert(GoogleCalculator.calculate("pi").startsWith("pi = 3.1415"))
+      assert(GoogleCalculator.calculate("pi", secure = false).startsWith("pi = 3.1415"))
+      assert(GoogleCalculator.calculate("pi", secure = true).startsWith("pi = 3.1415"))
     }
   }
 }
