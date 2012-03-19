8cf8826f1c35538348a16928486e7c116d1e2c15
Ricky Elrod <ricky@elrod.me>
2012-03-19 00:18:50 -0400

    Make it a bit simpler by using .data(), thus removing the need to url encode.

--- src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
+++ src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
@@ -1,5 +1,4 @@
 package me.elrod.GoogleCalculator
-import java.net.URLEncoder
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
@@ -7,9 +6,8 @@ object GoogleCalculator {
   System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20120222 Firefox/13.0a1")
 
   def calculate(query: String, secure: Boolean = true): String = {
-    val encodedQuery: String = URLEncoder.encode(query, "UTF-8").replace("+", "%20")
     val protocol = if (secure) "https" else "http"
-    val doc: Document = Jsoup.connect(protocol + "://www.google.com/search?q=" + encodedQuery).get()
+    val doc: Document = Jsoup.connect(protocol + "://www.google.com/search").data("q", query).get()
     doc.select("h2.r b").text
   }
 }
