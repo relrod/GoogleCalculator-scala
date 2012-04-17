75279abc375ec5cd2797f10d2a619457d119e550
Ricky Elrod <ricky@elrod.me>
2012-04-16 20:01:34 -0400

    Set the useragent using JSoup instead of globally which for some reason Travis CI doesn't like.

--- src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
+++ src/main/scala/me/elrod/GoogleCalculator/GoogleCalculator.scala
@@ -3,11 +3,10 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 object GoogleCalculator {
-  System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20120222 Firefox/13.0a1")
-
   def calculate(query: String, secure: Boolean = true): String = {
+    val userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20120222 Firefox/13.0a1"
     val protocol = if (secure) "https" else "http"
-    val doc: Document = Jsoup.connect(protocol + "://www.google.com/search").data("q", query).get()
+    val doc: Document = Jsoup.connect(protocol + "://www.google.com/search").data("q", query).userAgent(userAgent).get()
     doc.select("h2.r b").text
   }
 }
